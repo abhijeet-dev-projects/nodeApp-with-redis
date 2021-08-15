2fbd5eab70f3d66283524a5e37b19991e0abaaf5
abhijeet <slashkirkmetallica@gmail.com>
2021-08-15 17:52:59 +0530

    node-app cache-implementation on single branch

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://user:user@cluster0-shard-00-00.mqhjx.mongodb.net:27017,cluster0-shard-00-01.mqhjx.mongodb.net:27017,cluster0-shard-00-02.mqhjx.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-gza7ix-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
---
+++ services/cache.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function() {
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+    // check if a key exists in redis
+    const cachedValue = await client.get(key);
+
+    if(cachedValue){
+        console.log(cachedValue,'from cache')
+    }
+
+    // reach out to mongo
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result));
+
+    return result;
+
+}
+
