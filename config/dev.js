6df9b798d5a2e4e0d1aa1b6a50d9a4c3df7600d9
abhijeet-dev-projects <87030933+abhijeet-dev-projects@users.noreply.github.com>
2021-08-18 11:39:24 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://user:user@cluster0-shard-00-00.mqhjx.mongodb.net:27017,cluster0-shard-00-01.mqhjx.mongodb.net:27017,cluster0-shard-00-02.mqhjx.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-gza7ix-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  accessKeyId: 'AKIA2BJ22WVKNM3VORE2',
-  secretAccessKey: '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'
-};
