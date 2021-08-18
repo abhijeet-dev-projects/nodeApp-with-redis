405fa6ab7da322393c4e17b83f0a53ca67e10658
abhijeet <slashkirkmetallica@gmail.com>
2021-08-18 11:06:44 +0530

    pulling image from Db

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,13 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if(this.props.blog.imageUrl) {
+      // since we are getting only the image name from DB, prepend with domain name
+      return <img src={`my-image-bucket-domain${this.props.blog.imageUrl}`} />  
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
